d60f164345ca6598ccc522010d4d1ec08ae0bbf3
Christian Pojoni <dev@pojoni.at>
2020-07-04 20:25:51 +0200

    test question can be added

--- src/app/Http/Controllers/QuestionsController.php
+++ src/app/Http/Controllers/QuestionsController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
     public function store() {
-        
+        Question::create([
+            'title' => request('title')
+        ]);
     }
 }
